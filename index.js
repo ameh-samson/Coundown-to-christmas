f8625c7c7dfea6cbb18d7bb619a78400c25e1589
Samson Ameh <amehsam69@gmail.com>
2023-12-01 18:45:11 +0100

    Added seconds interval

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
 const countdownDisplay = document.getElementById("countdown-display");
-
-const currentDate = new Date();
+const dateAndTimeDisplay = document.getElementById("current-date");
+const christmasDay = 25;
 
 function renderCountdown() {
+  const currentDate = new Date();
   const currentDay = currentDate.getDate();
-  const christmasDay = 25;
 
-  const daytoChristmas =
+  const daysToChristmas =
     currentDay < christmasDay ? christmasDay - currentDay : christmasDay;
 
-  countdownDisplay.textContent = daytoChristmas;
+  countdownDisplay.textContent = daysToChristmas;
 }
-
-renderCountdown();
 
 function renderDateAndTime() {
-  const dateAndTime = document.getElementById("current-date");
-  const formats = {
-    day: "numeric",
-    month: "numeric",
-    year: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-    hour12: true,
-    timeZoneName: "short",
-  };
+  function updateDateTime() {
+    const currentDate = new Date();
+    const formats = {
+      day: "numeric",
+      month: "numeric",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: true,
+      timeZoneName: "short",
+    };
 
-  const formattedDateAndTime = currentDate.toLocaleString(undefined, formats);
-  dateAndTime.textContent = formattedDateAndTime;
+    const formattedDateAndTime = currentDate.toLocaleString(undefined, formats);
+    dateAndTimeDisplay.textContent = formattedDateAndTime;
+  }
+
+  // Initial rendering
+  updateDateTime();
+
+  // Update seconds every second
+  setInterval(updateDateTime, 1000);
 }
 
+// Initial rendering
+renderCountdown();
 renderDateAndTime();
